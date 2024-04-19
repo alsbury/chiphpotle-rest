795d618f7fe81ed77ce4d32a2864122a0d6ef812
Michael O'Connell <moconnell@citylifestyle.com>
2024-04-19 10:31:53 -0500

    update rector config to newer fluent syntax

--- rector.php
+++ rector.php
@@ -2,25 +2,14 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/test',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY,
-        SetList::TYPE_DECLARATION,
+    ])->withPhpSets()
+    ->withSets([
         PHPUnitSetList::PHPUNIT_100,
     ]);
-};
